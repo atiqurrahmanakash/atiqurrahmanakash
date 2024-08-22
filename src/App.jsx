a2c0853d7728a9ff97b2d6026881932e2006a666
atiqurrahmanakash <atiqurrahmanakash.developer@gmail.com>
2024-08-23 01:13:32 +0600

    update0.21

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <>
-      <h1><a href="/">Orebi</a></h1>
+      <h1><a href="https://orebi1.vercel.app/">Orebi</a></h1>
     </>
   )
 }
